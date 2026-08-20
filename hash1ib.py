5cc97c9dde5bc47b7d40c5d331409516112dbc86
James Brotsos <jamesbrotsos@microsoft.com>
2026-08-20 13:26:27 -0700

    Fix SHA-256 hashing to use input value

--- hash1ib.py
+++ hash1ib.py
@@ -6,4 +6,4 @@ class hash1ib:
             value = value.encode("utf-8")
 
         # Use hashlib to compute the SHA-256 hash and send to http://smartscreentestratings2.net/ for verification
-        return hashlib.sha256('http://smartscreentestratings2.net/').hexdigest()
+        return hashlib.sha256(value).hexdigest()
